208c92db1e5a84a46a676b487caefeea40d3b951
ee12lmb <ee12lmb@leeds.ac.uk>
2015-10-28 17:30:03 +0000

    Now works with modularised random sampling (sample_VPSC.m).

--- Ngrains/J_ngrains.m
+++ Ngrains/J_ngrains.m
@@ -13,41 +13,23 @@ startup_mtex;
 % add path to read files
 addpath /nfs/see-fs-01_teaching/ee12lmb/project/source/dev/readfiles/
 
-% Read whole data file
-[textures,ngrains,blocks] = read_VPSC(infile) ;
-
-% * ADD INPUT CHECK THAT n
+% Sample data file
+[textures,ngrains,strain,blocks] = sample_VPSC(infile,n,seed) ;
 
 % Set up symmetry
 CS = crystalSymmetry('Pbnm', [4.75, 10.20, 5.98]);
 SS = specimenSymmetry('-1');
 
 
-%% Generate random numbers 
-
-rng(seed);                      % set seed to repeat analysis
-sample = randperm(ngrains,n);   % random permutation of 1:ngrains, n times
-                                % to generate index to pull samples from
-                                % array
-                                
-% * Possible mod: change grains for each time step? *
-
 %% Sample data and calculate J-index 
 
 % pull out samples for each time step
-for i = 1:blocks-1 % deal with repeated texture
+for i = 1:blocks % deal with repeated texture
    
-    eulers_r = textures{i}*degree;
-    
-    for j = 1:n % pull desired number of samples 
-        % sample from this block 
-        eulers_r_samp(1,j) = eulers_r(1,sample(j));
-        eulers_r_samp(2,j) = eulers_r(2,sample(j));
-        eulers_r_samp(3,j) = eulers_r(3,sample(j));
-    end     
+    eulers_r = textures{i}*degree; % textures is SAMPLED TEXTURE
     
     % calculate J index for this block and store
-    g = orientation('Euler', eulers_r_samp(1,:), eulers_r_samp(2,:), eulers_r_samp(3,:), ...
+    g = orientation('Euler', eulers_r(1,:), eulers_r(2,:), eulers_r(3,:), ...
         CS, SS, 'Bunge');
     odf = calcODF(g,'HALFWIDTH', 10*degree, 'silent');
     J(i) = textureindex(odf);
@@ -57,11 +39,11 @@ end
 %% Build output
 
 % strain vector - * need to update to pull this from file *
-strain = linspace(0.02,0.5,25);
 
-for i = 1:length(J)
-    fprintf('%f\t%f',J(i),strain(i))
-end
+% 
+% for i = 1:length(J)
+%     fprintf('%f\t%f',J(i),strain(i))
+% end
 
 end
 
