9dd041170d32046f1873bf51e8bc0dab0e6f8d81
ee12lmb <ee12lmb@leeds.ac.uk>
2016-02-16 15:14:17 +0000

    Small function to find symmetry of given crystal

---
+++ readfiles/lookupSym.m
@@ -0,0 +1,27 @@
+function [ CS ] = lookupSym(crystal)
+%LOOKUPSYM finds symmetry for a given crystal
+
+switch lower(crystal)
+    case 'olivine'
+        
+        CS = crystalSymmetry('Pbnm', [4.75, 10.20, 5.98]);
+        
+    case 'post-perovskite'
+        
+        CS = crystalSymmetry('Cmcm', [2.47 8.09 6.11]);
+        
+    case 'quartz'
+        
+        CS = crystalSymmetry('pointId', 18, [4.913 4.913 5.504],...
+            [90,90,120]*degree,'X||a*', 'Y||b', 'Z||c', 'mineral',...
+            'Quartz','color', 'red');
+        
+    otherwise
+        
+        error('Input crystal not indexed')
+        
+end
+
+
+end
+
