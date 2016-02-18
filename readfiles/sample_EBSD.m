9ed05428c976006357937df1b6283c115c750550
ee12lmb <ee12lmb@leeds.ac.uk>
2016-02-18 17:11:43 +0000

    Function randomly samples from EBSD files

---
+++ readfiles/sample_EBSD.m
@@ -0,0 +1,32 @@
+function [ sampled_texture, ngrains ] = sample_EBSD(infile,crystal,n,seed)
+%SAMPLE_EBSD Returns a random sample of Euler angles from EBSD file
+%   Uses read_EBSD to get Euler angles from an EBSD (.ctf) file for a
+%   specific crystal phase defined by 'crystal'. This martix of Euler
+%   angles is then passed to sample_texture to return a matrix of n
+%   randomly sampled sets of Euler angles. Set the seed to allow repitition
+%   
+%   Inputs:     infile          - input EBSD file (must be .ctf)
+%               crystal         - string containing crystal phase name
+%               n               - number of grains to sample
+%               seed            - sets the random numbers to allow repeatability
+%
+%   Outputs:    sampled_texture - the sampled texture
+%               ngrains         - number of grains in the whole sample
+%
+
+
+%% Setup envrionment
+addpath /nfs/see-fs-01_teaching/ee12lmb/project/source/dev/
+setup_env;
+
+%% Read in and sample file
+
+% read in entire file
+eulers = read_EBSD(infile,crystal);
+
+% pass to sample texture to deal with 
+[sampled_texture, ~, ngrains] = sample_texture(eulers,n,seed);
+
+
+end
+
