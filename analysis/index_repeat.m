c213ddba37f8a77bed8027162226b210f4d51ae3
ee12lmb <ee12lmb@leeds.ac.uk>
2016-02-19 15:04:02 +0000

    IR can now work with EBSD files

--- analysis/index_repeat.m
+++ analysis/index_repeat.m
@@ -11,12 +11,17 @@ t = clock;
 %% Setup evironment
 
 addpath /nfs/see-fs-01_teaching/ee12lmb/project/source/dev/
-setup_env
+setup_env;
 
 %% check for optional arguments
 iarg = 1;
 wantout = 1; % we don't want output unless the 'filename' flag is active
 method = 0;  % default method to use is J-index
+
+% setup defautl symmetry (olivine)
+CS = crystalSymmetry('Pbnm', [4.75, 10.20, 5.98]);
+SS = specimenSymmetry('-1');
+crystal = 'olivine';
 
 while iarg<(length(varargin))
     switch lower(varargin{iarg})
@@ -49,7 +54,11 @@ while iarg<(length(varargin))
                     error('Unknown index')
             end
         
-        
+         case 'crystal'  % find the appropriate symmetry 
+            
+            iarg = iarg + 1; % take next argument
+            crystal = varargin{iarg};
+            CS = lookupSym(crystal);
         %-----------------------------------------------------------------
         otherwise
             error('Unknown flag')
@@ -64,15 +73,30 @@ end
 % check if input is raw VPSC or texture array
 if (ischar(input_texture) == 1)
     
-    % if a file path is given then read in
-    [textures,ngrains,strain,blocks] = read_VPSC(input_texture);
-    output = 0; % output format (see bottom of function)
+    % check file extension to see if EBSD
+    [~,~,ext] = fileparts(input_texture);
+    
+    if (strcmp(ext,'.ctf') == 1) % if input is path to EBSD file
+        
+        textures = read_EBSD(input_texture,crystal);
+        blocks = 1; % only ever one block in .ctf file
+        ngrains = length(textures);
+        output = 1;  % cannot output strain
+        
+    else % input must be VPSC
+
+        % if a file path is given then read in
+        [textures,ngrains,strain,blocks] = read_VPSC(input_texture);
+        output = 0; % output format (see bottom of function)
+    
+    end
     
 else
     
    % use sample_texture to gain relevant info about texture 
    [~,blocks,ngrains] = sample_texture(input_texture,1,1);
    textures = input_texture; % for consistency later
+   input_texture = 'Matlab matrix';
    output = 1;
  
 end
@@ -81,7 +105,7 @@ end
 %% Repeat index calculation for different sampled texture
 
 % assert that inputs are vaild
-assert((step < blocks),'Requested strain step is out of range of input texture')
+assert((step <= blocks),'Requested strain step is out of range of input texture')
 assert((n <= ngrains(1)),'Number of samples in the file is less than the number requested')
 
 % separate out correct block if texture is cell array (i.e. multiple blocks)
@@ -95,14 +119,13 @@ ini_seed = seed; % capture initial seed for output file
 for i = 1:repeat % loop over index calculation as many times as requested
 
     if (method == 0) % J-index
-        [ index(i), ~] = j_index(textures,n,seed);
+        [ index(i), ~] = j_index(textures,n,seed,'crystal',crystal);
         
     elseif (method == 1) % continuous M-index
-        [ index(i), ~] = m_indexCont(textures,n,seed);
+        [ index(i), ~] = m_indexCont(textures,n,seed,'crystal',crystal);
         
     elseif (method == 2) % discrete M-index
-        % need to sort this out
-        error('Discrete M-index not ready')
+        [ index(i), ~] = m_indexDisc(textures,n,seed,'crystal',crystal);
     end
     
     seed = seed + 1; % now sample with a different seed 
@@ -116,7 +139,7 @@ time = toc;
 if (wantout == 0) % if the filepath has been given as an option
     
     % assume that filepath checked in shell script/matlab can handle this
-    fid = fopen(outfile,'a'); % open file for writing (append, so can add headers in shell)
+    fid = fopen(outfile,'w'); % open file for writing 
 
     switch output
         case 0     % our input was a file path so we know strain
@@ -129,11 +152,12 @@ if (wantout == 0) % if the filepath has been given as an option
             fprintf(fid,'+Index:\t\t%s\n',lower(index_check));
             fprintf(fid,'+Strain step:\t%i\n',step);
             fprintf(fid,'+Strain:\t%f\n',strain(step));
+            fprintf(fid,'+Crystal:\t%s\n',crystal);
             fprintf(fid,'+Grains:\t%i\n',n);
             fprintf(fid,'+Seed:\t\t%i\n',ini_seed);
             fprintf(fid,'+Repeat:\t%i\n',repeat);
             fprintf(fid,'+Time taken(s):\t%f\n',time);
-            fprintf(fid,'+Columns:\tIndex\n\n');
+            fprintf(fid,'+Columns:\tIndex\n');
             fprintf(fid,'Data\n');
 
             for i = 1:length(index)
@@ -146,15 +170,16 @@ if (wantout == 0) % if the filepath has been given as an option
             fprintf(fid,'IR\t%i\n',length(index)); % code for read_texout 
             fprintf(fid,'+Function:\tindex_repeat\n');
             fprintf(fid,'+Time/date:\t%i:%i %i/%i/%i\n',t(4),t(5),t(3),t(2),t(1));
-            fprintf(fid,'+Input file:\tn/a\n');
+            fprintf(fid,'+Input file:\t%s\n',input_texture);
             fprintf(fid,'+Index:\t\t%s\n',lower(index_check));
             fprintf(fid,'+Strain step:\t%i\n',step);
             fprintf(fid,'+Strain:\tn/a\n');
+            fprintf(fid,'+Crystal:\t%s\n',crystal);
             fprintf(fid,'+Grains:\t%i\n',n);
             fprintf(fid,'+Seed:\t\t%i\n',ini_seed);
             fprintf(fid,'+Repeat:\t%i\n',repeat);
             fprintf(fid,'+Time taken(s):\t%f\n',time);
-            fprintf(fid,'+Columns:\tIndex\n\n');
+            fprintf(fid,'+Columns:\tIndex\n');
             fprintf(fid,'Data\n');
 
             for i = 1:length(index)
