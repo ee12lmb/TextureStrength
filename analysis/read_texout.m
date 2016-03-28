9c44df72e141411469d956a609e5f75dc411f8a3
ee12lmb <ee12lmb@leeds.ac.uk>
2016-03-29 00:34:44 +0100

    Added ability to extract some header information

--- analysis/read_texout.m
+++ analysis/read_texout.m
@@ -1,7 +1,22 @@
-function [ data,type ] = read_texout(file)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
+function [ data, type, header ] = read_texout(file)
+%READ_TEXOUT Reads in data from output of an index calculation
+%   Data files outputted by j_index, m_indexCont, m_indexDisc and
+%   index_repeat can be read in using read_texout. Header information is
+%   also extracted from these files.
+%
+%   Inputs:     file   - file outputted from other texture function
+%
+%   Outputs:    data   - array of data read from file, either two columns
+%                        where strain is available, or a single column for 
+%                        all other cases
+%
+%               type   - label at the top of output file that represents the
+%                        data within the file e.g. MD2 is an output from
+%                        m_indexDisc with two columns (strain | index)
+%
+%               header - meta data extracted from data file
+%                    
+%   Usage: [ data, type, header ] = read_texout(file)
 
 %% setup environment
 addpath /nfs/see-fs-01_teaching/ee12lmb/project/source/dev/
@@ -24,18 +39,40 @@ length = info{2};
 switch type
     case 'IR' % reading in index repeat file
         
-        for i = 1:14 % skip headers
-            fgetl(fid);
+        for i = 1:14 % capture headers
+            header_tmp{i} = fgetl(fid);
         end
 
+        % extract useful header info
+        fname     = textscan(header_tmp{4},'%s %s %s');
+        index     = textscan(header_tmp{5},'%s %s %s %s %s %s %s');
+        xtal      = textscan(header_tmp{8},'%s %s');
+        numgrains = textscan(header_tmp{9},'%s %d %s %s %s %s');
+        repeat    = textscan(header_tmp{11},'%s %d');
+        
+        header{1} = fname{3};
+        header{2} = xtal{2};
+        header{3} = numgrains{2};
+        header{4} = index{2};
+        header{5} = repeat{2};
+        
         % read in data
         data = fscanf(fid, '%g', [length 1]);
         
     case {'MC2','MD2','J2'} % reading in two column index output file
         
-        for i = 1:10 % skip headers
-            fgetl(fid);
+        for i = 1:10 % capture headers
+            header_tmp{i} = fgetl(fid);
         end
+        
+        % extract useful header info
+        fname     = textscan(header_tmp{4},'%s %s %s');
+        xtal      = textscan(header_tmp{5},'%s %s');
+        numgrains = textscan(header_tmp{6},'%s %d %s %s %s %s');
+        
+        header{1} = fname{3};
+        header{2} = xtal{2};
+        header{3} = numgrains{2};
         
         % read in data
         data = fscanf(fid, '%g %g', [2 length]);
@@ -44,8 +81,17 @@ switch type
     case {'MC1','MD1','J1'} % reading in single column index output file
         
         for i = 1:10
-            fgetl(fid);
+            header_tmp{i} = fgetl(fid);
         end
+        
+        % extract useful header info
+        fname     = textscan(header_tmp{4},'%s %s %s');
+        xtal      = textscan(header_tmp{5},'%s %s');
+        numgrains = textscan(header_tmp{6},'%s %d %s %s %s %s');
+        
+        header{1} = fname{3};
+        header{2} = xtal{2};
+        header{3} = numgrains{2};
         
         % read in data
         data = fscanf(fid, '%g', [length 1]);
