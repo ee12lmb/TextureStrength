69c7fb3ba87b5fd18ea7558e1a2386df5bfb9d2c
ee12lmb <ee12lmb@leeds.ac.uk>
2016-04-13 15:07:18 +0100

    Function simply lists available functions in toolbox

---
+++ toolbox_functions.m
@@ -0,0 +1,16 @@
+function toolbox_functions()
+%
+%  Available functions:
+%
+%  Read/sample textures         Index calculation       Analysis
+%  ------------------------------------------------------------------------
+%  > read_VPSC                  > j_index               > index_repeat
+%  > sample_VPSC                > m_indexCont           > get_strain
+%  > read_EBSD                  > m_indexDisc           > calc_anisotropy
+%  > sample_EBSD                                        > read_texout
+%  > sample_texture
+%
+%  Type help [ function ] for details.
+%
+end
+
